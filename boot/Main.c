410baa584911e5930fbcb7f9b83f3465de663cf7
kiles <ordajoy@gmail.com>
2024-11-26 16:09:57 +0900

    6.2절

--- boot/Main.c
+++ boot/Main.c
@@ -1,5 +1,6 @@
 #include "stdint.h"
 #include "HalUart.h"
+#include "HalInterrupt.h"
 #include "stdio.h"
 
 static void Hw_init(void);
@@ -24,6 +25,7 @@ void main(void)
 
 static void Hw_init(void)
 {
+    Hal_interrupt_init();
     Hal_uart_init();
 }
 
